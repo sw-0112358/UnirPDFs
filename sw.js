bdcff8aa3487fe4d006852f9aeb1fee441568858
0112358 <gx0112358@gmail.com>
2026-06-10 01:06:26 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 🔧 Configuración: cambia solo estas dos constantes
 const PROJECT_NAME = "UnirPDFs"; // nombre del proyecto
-const VERSION = "3";                 // versión actual
+const VERSION = "4";                 // versión actual
 
 const CACHE_NAME = `${PROJECT_NAME}-cache-${VERSION}`;
 const FILES_TO_CACHE = [
